a468bb4a5c11479c96e1ebe6b41b5c6175801274
alexcht004 <56443132+alexcht004@users.noreply.github.com>
2020-05-22 04:23:41 +0200

    Add files via upload

---
+++ numero1/Datos.java
@@ -0,0 +1,21 @@
+package numero1;
+
+public class Datos {
+	String nombre;
+	int fechaInicio ;
+	String valoracion;
+    public void darNombre(){
+    	
+    	nombre = "diego";
+    }
+    public void darValoracion(){
+    	
+    	valoracion = " alta ";
+    }
+    public void mostrarDatos(){
+    	
+    	System.out.println (""+valoracion);
+    	System.out.println (""+nombre);
+    }
+    
+}
---
+++ numero1/main.java
@@ -0,0 +1,32 @@
+package numero1;
+
+public class main {
+    public static void main(String[] args) {
+        //OBJETO
+        Concursante concursante = new Concursante();
+        
+        
+        
+        
+        Datos datos = new Datos();
+        Directivos directivos = new Directivos();
+        Dueño dueño = new Dueño();
+
+        Personal personal = new Personal();
+        Personas personas = new Personas();
+        Usuarios usuarios = new Usuarios();
+        PaginaWeb paginaWeb = new PaginaWeb();
+
+        //OBJETO EQUIPACIONES
+        UsuarioVip usuarioVip = new UsuarioVip();
+        
+       
+
+      
+        //MOSTRAR METODOS
+        usuarioVip.darFechaSalida();
+        usuarioVip.cambiarCategoria();
+        usuarioVip.mostrarDatos();
+
+    }
+}
